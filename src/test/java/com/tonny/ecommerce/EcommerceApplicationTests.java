71570821a10fb11cddc14f88be80cc72207cdee6
Anthonny Gabriell Marins Alves <anthonny.alves@sefaz.mt.gov.br>
2026-07-08 07:54:09 -0400

    First commit

---
+++ src/test/java/com/tonny/ecommerce/EcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tonny.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
