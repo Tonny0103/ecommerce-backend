be3055d25e2fe37dc4f16e6dc126226202f84b76
Anthonny Gabriell Marins Alves <anthonny.alves@sefaz.mt.gov.br>
2026-08-14 09:17:11 -0400

    feat(GlobalExceptionHandler): creating handlers for product not found and already exists

--- src/main/java/com/tonny/ecommerce/GlobalExceptionHandler.java
+++ src/main/java/com/tonny/ecommerce/GlobalExceptionHandler.java
@@ -1,6 +1,8 @@
 package com.tonny.ecommerce;
 
 import com.tonny.ecommerce.DTO.ErrorResponseDTO;
+import com.tonny.ecommerce.exception.ProductAlreadyExistsException;
+import com.tonny.ecommerce.exception.ProductNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -10,6 +12,28 @@ import java.time.LocalDateTime;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ProductNotFoundException.class)
+    public ResponseEntity<ErrorResponseDTO> handleProductNotFound(ProductNotFoundException ex) {
+        ErrorResponseDTO erro = new ErrorResponseDTO(
+                HttpStatus.NOT_FOUND.value(),
+                "Product Not Found",
+                ex.getMessage(),
+                LocalDateTime.now()
+        );
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(erro);
+    }
+
+    @ExceptionHandler(ProductAlreadyExistsException.class)
+    public ResponseEntity<ErrorResponseDTO> handleProductAlreadyExists(ProductAlreadyExistsException ex) {
+        ErrorResponseDTO erro = new ErrorResponseDTO(
+                HttpStatus.BAD_REQUEST.value(),
+                "Product Already Exists",
+                ex.getMessage(),
+                LocalDateTime.now()
+        );
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(erro);
+    }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponseDTO> handleGenericError(Exception ex) {
